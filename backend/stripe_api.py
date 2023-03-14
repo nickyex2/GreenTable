a6c7026fa348033b971a8bab439e3a004b70b81c
Nicholas Goh <nicholasgbr99@gmail.com>
2023-03-14 21:32:02 +0800

    stripe api updates

--- backend/stripe_api.py
+++ backend/stripe_api.py
@@ -1,21 +1,37 @@
+from flask import Flask, jsonify, request
+from flask_cors import CORS
 import stripe
 from dotenv import load_dotenv
 import os
 load_dotenv()
-stripe.api_key = os.getenv("STRIPE_API_KEY")
+stripe.api_key = os.getenv("STRIPE_SECRET_KEY")
 
-# create a paymentmethod
-stripe.PaymentMethod.create(
-    type="card",
-    card={
-        "number": "4242424242424242",
-        "exp_month": 8,
-        "exp_year": 2020,
-        "cvc": "314",
-    }
-)
+app = Flask(__name__)
+CORS(app)
 
-#create a paymentintent
-stripe.PaymentIntent.create(
-    amount=1000,
-    currency='sgd')
+@app.route('/api/payment', methods=['POST'])
+def payment():
+    # create a paymentmethod
+    pm_string = stripe.PaymentMethod.create(
+        type="card",
+        card={
+            "number": "4000000000000002",
+            "exp_month": 8,
+            "exp_year": 2025,
+            "cvc": "314",
+        }
+    )
+    #create a paymentintent and make payment straight away
+    try:
+        result = stripe.PaymentIntent.create(
+            amount=10000,
+            currency='sgd',
+            confirm=True,
+            payment_method=pm_string['id'])
+        return jsonify(result), 200
+    except Exception as e:
+        return {"message": f'{e}'}, 400
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5016, host='0.0.0.0')
+
