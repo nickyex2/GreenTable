98cc399381795202a848d85c81f0261a9362896e
Nicklaus Chiok <tschiok.2021@scis.smu.edu.sg>
2023-03-15 16:12:17 +0800

    catalog mongoDB

--- backend/Catalog.py
+++ backend/Catalog.py
@@ -1,18 +1,18 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import requests
-
-import firebase_admin
-from firebase_admin import credentials
-from firebase_admin import firestore
-
-cred = credentials.Certificate("./esd-project-g6-firebase-adminsdk.json")
-firebase_admin.initialize_app(cred)
-
-db = firestore.client()
+import pymongo
+from bson.json_util import dumps, loads
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
 Catalog = Flask(__name__)
 CORS(Catalog)
+
+client = pymongo.MongoClient(os.environ.get('CATALOG_DB_URL'))
+db = client["Catalog"]
+collection = db["Catalog"]
 
 
 @Catalog.route('/catalog/<string:query>')
@@ -29,15 +29,128 @@ def search(query):
 
     return response_json['candidates'][0]
 
+@Catalog.route('/catalog/add', methods=['POST'])
+def addRestaurant():
+    json = request.get_json()
+    collection.insert_one(json)
+    return "Added"
+
 @Catalog.route('/catalog/all', methods=['GET'])
 def getAll():
-    doc_ref = db.collection(u'Restaurants').stream()
-    
-    restaurants = []
-    for doc in doc_ref:
-        restaurants.append(doc.to_dict())
+    items = collection.find({ })
+    result = loads(dumps(items))
+    return result, 200
 
-    return jsonify(restaurants)
+"""
+{
+    "_id": "Elemen",
+    "location": {
+        "formatted_address": "9 Raffles Blvd #01-75/75A/76 Millenia Walk",
+        "postal_code": "039596"
+    },
+    "description": "elemen is a dining concept focused on a modern interpretation of natural and meatless cuisine. It is dedicated to fostering the well-being of our customers through our wholesome meatless recipes and use of natural ingredients. Our elemen restaurants offer a pleasant dining experience and our brand motto 'Love Self, Love Earth' explains our focus of serving healthy and natural food, without compromising the sustainability of the environment.",
+    "category": "category",
+    "image": ["image"],
+    "website": "elemengroup.com.sg",
+    "cuisine": ["Vegetarian"],
+    "availability": {
+        "270322": {
+            "1100": 5,
+            "1200": 5,
+            "1300": 5,
+            "1400": 5,
+            "1500": 5,
+            "1600": 5,
+            "1700": 5,
+            "1800": 5,
+            "1900": 5,
+            "2000": 5,
+            "2100": 5
+        },
+        "280322": {
+            "1100": 5,
+            "1200": 5,
+            "1300": 5,
+            "1400": 5,
+            "1500": 5,
+            "1600": 5,
+            "1700": 5,
+            "1800": 5,
+            "1900": 5,
+            "2000": 5,
+            "2100": 5
+        },
+        "290322": {
+            "1100": 5,
+            "1200": 5,
+            "1300": 5,
+            "1400": 5,
+            "1500": 5,
+            "1600": 5,
+            "1700": 5,
+            "1800": 5,
+            "1900": 5,
+            "2000": 5,
+            "2100": 5
+        },
+        "300322": {
+            "1100": 5,
+            "1200": 5,
+            "1300": 5,
+            "1400": 5,
+            "1500": 5,
+            "1600": 5,
+            "1700": 5,
+            "1800": 5,
+            "1900": 5,
+            "2000": 5,
+            "2100": 5
+        },
+        "310322": {
+            "1100": 5,
+            "1200": 5,
+            "1300": 5,
+            "1400": 5,
+            "1500": 5,
+            "1600": 5,
+            "1700": 5,
+            "1800": 5,
+            "1900": 5,
+            "2000": 5,
+            "2100": 5
+        },
+        "010422": {
+            "1100": 5,
+            "1200": 5,
+            "1300": 5,
+            "1400": 5,
+            "1500": 5,
+            "1600": 5,
+            "1700": 5,
+            "1800": 5,
+            "1900": 5,
+            "2000": 5,
+            "2100": 5
+        },
+        "020422": {
+            "1100": 5,
+            "1200": 5,
+            "1300": 5,
+            "1400": 5,
+            "1500": 5,
+            "1600": 5,
+            "1700": 5,
+            "1800": 5,
+            "1900": 5,
+            "2000": 5,
+            "2100": 5
+        }
+    },
+    "avg_rating": 4.0,
+    "number_of_ratings": 893
+}
+
+"""
 
 @Catalog.route('/catalog/updateAvailability', methods=['PUT'])
 def updateAvailability():
@@ -48,27 +161,24 @@ def updateAvailability():
         return "Request was not JSON"
 
 def processUpdateAvailability(obj):
-    name = obj['name']
-    size = obj['size']
-    status = obj['status']
+    restaurant_name = obj['r_name']
+    # customer_name = obj['c_name']
+    date = obj['date']
+    time = obj['time']
 
-    doc_ref = db.collection(u'Restaurants').document(name)
-    curr = doc_ref.get()
-    updated_avail = 0
+    doc = collection.find_one({"_id": restaurant_name})
 
-    result = curr.to_dict()
+    # Check if the availability for the specified date and time is greater than 0
+    if doc["availability"][date][time] > 0:
+        # Subtract 1 from the availability for the specified date and time
+        doc["availability"][date][time] -= 1
+        # Update the document in the database
+        collection.update_one({"_id": "Elemen"}, {"$set": {"availability": doc["availability"]}})
+    else:
+        return "No availability"
 
-    if status == 'entering':
-        if result['availability'] > size:
-            updated_avail += result['availability'] - size
-        else:
-            return "Not enough space"
-    elif status == 'leaving':
-        updated_avail += result['availability'] + size
 
-    doc_ref.update({'availability': updated_avail})
-
-    return "Update Successful"        
+    return "Updated"
 
 if __name__ == '__main__':
     Catalog.run(port=5002, debug=True)
